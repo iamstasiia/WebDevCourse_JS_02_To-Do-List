c92a208619b3b1c032d0669944ac61d39315735c
Anastasiia <anastasiia.khrypko@dci-student.org>
2024-04-09 09:25:30 +0200

    add complete: false

--- tasks.js
+++ tasks.js
@@ -2,9 +2,11 @@ export const tasks = [
   {
     task: "Beenden Sie das Programm",
     datum: "09/04/2024",
+    complete: false,
   },
   {
     task: "Machen Sie eine erfolgreiche Präsentation",
     datum: "10/04/2024",
+    complete: false,
   },
 ];
